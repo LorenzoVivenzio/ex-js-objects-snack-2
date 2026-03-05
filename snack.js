7dc252e6e5999ede2a354facbd7e539b64fcce09
LorenzoVivenzio <lorenzovivenzio988@hotmail.com>
2026-03-05 16:49:40 +0100

    first commit

---
+++ snack.js
@@ -0,0 +1,7 @@
+// const hamburger = { name: "Cheese Burger", weight: 250 };
+// const secondBurger = hamburger;
+// secondBurger.name = 'Double Cheese Burger';
+// secondBurger.weight = 500;
+
+// console.log(hamburger); // Double Cheese Burger
+// console.log(secondBurger ); // Double Cheese Burger
---
+++ snack4.js
@@ -0,0 +1,21 @@
+const chef = {
+	name: "Chef Hyur",
+	age: 29,
+	makeBurger: (num = 1) => {
+		console.log(`Ecco ${num} hamburger per te!`);
+	},
+}
+
+const restaurant = {
+	name: "Hyur's Burgers",
+	address: {
+		street: 'Main Street',
+		number: 123,
+	},
+	openingDate: new Date(2025, 3, 11),
+	isOpen: false,
+};
+
+
+const restaurantClone = {...restaurant}
+console.log(restaurantClone)
